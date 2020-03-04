9535e59d5c848a85a5e73bc2192cc6f294e1f8a6
duro06 <andi.meka.025@gmail.com>
2020-03-04 19:39:11 +0700

    Install bulma-badge dan inisialisasi main. js

--- src/main.js
+++ src/main.js
@@ -4,10 +4,43 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import Vuex from "vuex";
+import axios from "axios";
+import vWow from "v-wow";
+import FlashMessage from "@smartweb/vue-flash-message";
+import VueCarousel from "vue-carousel";
+import infiniteScroll from "vue-infinite-scroll";
+
+import "bulma-badge/dist/css/bulma-badge.min.css";
+
+Vue.use(Vuex, axios);
+// Vue.use(axios);
+Vue.use(vWow);
+Vue.use(FlashMessage);
+Vue.use(VueCarousel);
+Vue.use(infiniteScroll);
+// Vue.use(BulmaCarousel)
+
+// const _urlOriginApi = "http://192.168.43.231/capcin/";
+const _LurlApi = "http://localhost:8000/api/";
+// const _LurlApi = "http://localhost/capcin/";
+// const _LurlApi = "http://192.168.1.100/capcin/";
+// const _LurlApi = "http://localhost/capcin-copy/";
+// const _newUrlApp = _LurlApi + "app";
+// const _newUrlUser = _LurlApi + "user/login";
+// const _newUrlApiLogin = _LurlApi + "apilogin";
+
+axios.defaults.baseURL = _LurlApi;
+
+window.addEventListener("beforeunload", function() {
+  localStorage.setItem("level", store.getters.levelAccess);
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  axios,
   render: h => h(App)
 }).$mount("#app");
